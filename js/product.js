cc5a7b24dc493dc61388ecad129a1b5798a34dcd
devjuju <jleleubellpro1994@gmail.com>
2023-01-14 15:04:57 +0100

    javascript terminé

--- js/product.js
+++ js/product.js
@@ -1,58 +1,144 @@
-const host = "https://kanapjmax.herokuapp.com/";
-const getUrl = host + "api/products/";
-fetch(getUrl)
-.then(function (res) {
-  if (res.ok) {
-    return res.json();
-  }
-})
-.then(function (value){
-  for (let i of value) {   
-    let img = document.querySelector(".item__img");
-    img.innerHTML = "<img src="+i.imageUrl+"alt="+'"'+i.altTxt+'"'+">";
-  
-    let name = document.getElementById("title");
-    name.innerHTML =  i.name;
+var str = window.location.href;
+var url = new URL(str);
+var idProduct = url.searchParams.get("id");
+console.log(idProduct);
+let sofa = "";
 
-    let price = document.getElementById("price");
-    price.innerHTML = i.price;
+const choiceColor = document. querySelector("#colors");
+const choiceQuantity = document.querySelector("#quantity");
 
-    let description = document.getElementById("description");
-    description.innerHTML = i.description;
- 
-    let color = document.getElementById("colors");
-    for (let j of i.colors) {
-      color.innerHTML += "<option value="+j+">"+j+"</option>";
-    }
-      }
+getSofa();
+
+// Récupération des articles de l'API
+function getSofa() {
+    fetch("http://localhost:3000/api/products/" + idProduct)
+    .then((res) => {
+        return res.json();
+    })
+
+    // Répartition des données de l'API dans le DOM
+    .then(async function (resultatAPI) {
+        sofa = await resultatAPI;
+        console.table(sofa);
+        if (sofa){
+            getPost(sofa);
+        }
     })
     .catch((error) => {
-      console.log(error);
-    });
-      
-productsFetch();
+        console.log("Erreur de la requête API");
+    })
+}
+    
+function getPost(sofa){
+    let img = document.querySelector(".item__img");
+    img.innerHTML = "<img src="+sofa.imageUrl+" alt="+'"'+sofa.altTxt+'"'+">";
+          
+    let name = document.getElementById("title");
+    name.innerHTML =  sofa.name;
+        
+    let price = document.getElementById("price");
+    price.innerHTML = sofa.price;
+        
+    let description = document.getElementById("description");
+    description.innerHTML = sofa.description;
 
-function qtyValue() {
-  let qty = document.getElementById("quantity");
-  return qty.value;
+    // Insertion des options de couleurs
+    
+    for (let colors of sofa.colors){
+        console.table(colors);
+        let productColors = document.createElement("option");
+        document.querySelector("#colors").appendChild(productColors);
+        productColors.value = colors;
+        productColors.innerHTML = colors;
+    }
+  
 }
 
-function colorValue() {
-  let color = document.getElementById("colors");
-  return color.value;
-}
+//Gestion du panier
+ // événement d'appel au clic
+ joinEvent();
+ // Fonction pour joindre un événement
+ function joinEvent(){
+     const button_addToCart = document.querySelector("#addToCart");
+     button_addToCart.addEventListener("click",  (event)=>{
+         
+         addToCart(sofa)
+     })
+ }
 
-const toCartBtn = document.getElementById("addToCart");
-const goToCartButton = document.getElementById("goToCart");
-goToCartButton.style.display = "none";
 
-toCartBtn.addEventListener("click", () => {
-  let qty = parseInt(qtyValue());
-  let color = colorValue();
-  add2Cart(id, color, qty);
-  goToCartButton.style.display = "block";
-});
+ // Cette fonction permet d'enregistrer le panier dans le localStorage
+ // L'idée est d'enregistrer une valeur par rapport à une clé
+ function saveCart(Storage){
+    localStorage.setItem("produit",JSON.stringify(Storage));
+  }
 
-goToCartButton.addEventListener("click", () => {
-  window.location.href = "./cart.html";
-});
+ 
+
+  function addToCart(sofa){
+    if (choiceQuantity.value > 0 && choiceQuantity.value <=100 && choiceQuantity.value != 0){
+
+        //Recupération du choix de la couleur
+        let choixCouleur = choiceColor.value;
+                    
+        //Recupération du choix de la quantité
+        let choixQuantite = choiceQuantity.value;
+    
+        //Récupération des options de l'article à ajouter au panier
+        let optionsProduit = {
+            idProduit: idProduct,
+            couleurProduit: choixCouleur,
+            quantiteProduit: Number(choixQuantite),
+            nomProduit: sofa.name,
+            prixProduit: sofa.price,
+            descriptionProduit: sofa.description,
+            imgProduit: sofa.imageUrl,
+            altImgProduit: sofa.altTxt
+        };
+    
+   
+    
+        //fenêtre pop-up
+        const popupConfirmation =() =>{
+            if(window.confirm(`Votre commande de ${choixQuantite} ${sofa.name} ${choixCouleur} est ajoutée au panier
+    Pour consulter votre panier, cliquez sur OK`)){
+                window.location.href ="cart.html";
+            }
+        }
+        let Storage = JSON.parse(localStorage.getItem("produit"));
+        //Importation dans le local storage
+        //Si le panier comporte déjà au moins 1 article
+        if (Storage) {
+        let resultFind = Storage.find(el => el.idProduit === idProduct && el.couleurProduit === choixCouleur);
+            //Si le produit commandé est déjà dans le panier
+            if (resultFind != undefined) {
+                let newQuantite =
+                parseInt(optionsProduit.quantiteProduit) + parseInt(resultFind.quantiteProduit);
+                resultFind.quantiteProduit = newQuantite;
+                saveCart(Storage);
+                console.table(Storage);
+                popupConfirmation();
+            //Si le produit commandé n'est pas dans le panier
+            } else {
+                Storage.push(optionsProduit);
+                saveCart(Storage);
+                console.table(Storage);
+                popupConfirmation();
+            }
+        //Si le panier est vide
+        } else {
+            emptyCart();
+            Storage.push(optionsProduit);
+            console.table(Storage);
+            popupConfirmation();
+        }}
+  }  
+
+  function emptyCart(){
+    let Storage = localStorage.getItem("produit");
+    if(Storage == null){
+        Storage =[];
+    } else{
+        return JSON.parse(Storage);
+    }
+ }
--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
-const host = "https://kanapjmax.herokuapp.com/";
-const getUrl = host + "api/products/";
-  fetch(getUrl)
-.then(function (res) {
-  if (res.ok) {
-    return res.json();
-  }
-})
-.then(function (value){
-  for (let i of value) {
-    document.getElementById("items").innerHTML += 
-          "<a href= "+"./product.html?id="+i._id+">"+
+fillSection();
+
+// Récupération des articles de l'API
+async function getSofas() {
+    var sofasCatch = await fetch("http://localhost:3000/api/products")
+    return await sofasCatch.json();
+}
+
+    // Répartition des données de l'API dans le DOM
+async function fillSection() {
+    var result = await getSofas ()
+    .then(function (resultatAPI){
+        const sofas = resultatAPI;
+        console.table(sofas);
+        for (let sofa of sofas) {
+
+          document.getElementById("items").innerHTML += 
+          "<a href= "+"./product.html?id="+sofa._id+">"+
             "<article>"+
-             "<img src="+i.imageUrl+"alt="+'"'+i.altTxt+'"'+">"+
-              "<h3 class="+"productName"+">"+i.name+"</h3>"+
+             "<img src="+sofa.imageUrl+" alt="+'"'+sofa.altTxt+'"'+">"+
+              "<h3 class="+"productName"+">"+sofa.name+"</h3>"+
               "<p class="+"productDescription"+">"+
-                i.description+
+                sofa.description+
               "</p>"+
             "</article>"+
           "</a>"
-        ;
-      }
+        }
     })
-    .catch((error) => {
-      console.log(error);
+    .catch (function(error){
+        return error;
     });
-      
-
-
+}
