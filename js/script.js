99c999bef50b4fd0bc10a91a40029d313d425fec
devjuju <jleleubellpro1994@gmail.com>
2023-02-07 10:12:04 +0100

    Étape 3 : Insérer les produits dans la page d’accueil

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ fillSection();
 
 // Récupération des articles de l'API
 async function getSofas() {
-  var sofasCatch = await fetch("http://localhost:3000/api/products")
-  return await sofasCatch.json();
+    var sofasCatch = await fetch("http://localhost:3000/api/products")
+    return await sofasCatch.json();
 }
 
 // Répartition des données de l'API dans le DOM
@@ -12,20 +12,37 @@ async function fillSection() {
     .then(function (resultatAPI){
         const sofas = resultatAPI;
         console.table(sofas);
-        for (let sofa of sofas) {
-          document.getElementById("items").innerHTML += 
-          "<a href= "+"./product.html?id="+sofa._id+">"+
-            "<article>"+
-             "<img src="+sofa.imageUrl+" alt="+'"'+sofa.altTxt+'"'+">"+
-              "<h3 class="+"productName"+">"+sofa.name+"</h3>"+
-              "<p class="+"productDescription"+">"+
-                sofa.description+
-              "</p>"+
-            "</article>"+
-          "</a>"
+        for (let sofa in sofas) {
+
+            // Insertion de l'élément "a"
+            let articleLink = document.createElement("a");
+            document.querySelector(".items").appendChild(articleLink);
+            articleLink.href = `product.html?id=${resultatAPI[sofa]._id}`;
+
+            // Insertion de l'élément "article"
+            let articleSofa = document.createElement("article");
+            articleLink.appendChild(articleSofa);
+
+            // Insertion de l'image
+            let articleImg = document.createElement("img");
+            articleSofa.appendChild(articleImg);
+            articleImg.src = resultatAPI[sofa].imageUrl;
+            articleImg.alt = resultatAPI[sofa].altTxt;
+
+            // Insertion du titre "h3"
+            let articleName = document.createElement("h3");
+            articleSofa.appendChild(articleName);
+            articleName.classList.add("productName");
+            articleName.innerHTML = resultatAPI[sofa].name;
+
+            // Insertion de la description "p"
+            let articleDescription = document.createElement("p");
+            articleSofa.appendChild(articleDescription);
+            articleDescription.classList.add("productName");
+            articleDescription.innerHTML = resultatAPI[sofa].description;
         }
-      })
-  .catch (function(error){
-    return error;
-  });
+    })
+    .catch (function(error){
+        return error;
+    });
 }
